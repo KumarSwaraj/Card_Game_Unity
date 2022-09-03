3e418718583c807eb59fe9b2a12372ede270a210
Swayam-shree-06 <swarajswain00@gmail.com>
2022-09-03 18:55:00 +0530

    Sample Commit

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    public GameObject Canvas;
+    private bool isDragging = false;
+    private bool isOverDropZone = false;
+    private GameObject dropZone;
+    private GameObject startParent;
+    private Vector3 startPosition;
+
+    private void Awake()
+    {
+        Canvas = GameObject.Find("Main Canvas");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);   
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropZone = null;
+    }
+
+    public void StartDrag()
+    {
+        startParent = transform.parent.gameObject; //when start this dragging process, then to store wherever the parent is into this variable called 'startParent'
+        startPosition = transform.position;
+        isDragging = true;
+    }
+    
+    public void EndDrag()
+    {
+        isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }
+        else
+        {
+            //if it isn't over a dropzone then need to be the transform.position to be go back to its start position
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
+    }
+}
---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject Card3;
+    public GameObject Card4;
+    public GameObject PlayerArea;
+    public GameObject OpponentArea;
+
+    List<GameObject> cards = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cards.Add(Card1);
+        cards.Add(Card2);
+        cards.Add(Card3);
+        cards.Add(Card4);
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            //in the card list, select a random index a random within that list and range of that will be between 0 & the no. of things in that list
+            GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+            playerCard.transform.SetParent(PlayerArea.transform, false);
+
+            GameObject opponentCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+            opponentCard.transform.SetParent(OpponentArea.transform, false);
+        }
+    }
+}
